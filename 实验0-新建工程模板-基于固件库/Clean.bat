0c3f9674f1ee695b046cbd284de13873c1ed7f44
dongchao612 <dongchao980612@163.com>
2025-10-28 18:31:45 +0800

    first  project

---
+++ 实验0-新建工程模板-基于固件库/Clean.bat
@@ -0,0 +1,21 @@
+
+::打开回显
+@echo on
+
+::删除文件
+del=/s /a /f "JLinkLog.txt"
+del=/s /a /f "*.ini"
+del=/s /a /f "*.uvguix.*"
+del=/s /a /f "*.uvoptx.*"
+del=/s /a /f "*.scvd"
+
+::删除 "DebugConfig" "Listings" "Objects"文件夹
+for /f "delims=" %%i in ('dir /ad /b /s "DebugConfig" "Listings" "OBJ" "Objects"') do (
+   rd /s /q "%%i"
+)
+
+
+::删除空文件夹
+::for /f "tokens=*" %%i in ('dir/s/b/ad^|sort /r') do rd "%%i"
+
+exit
